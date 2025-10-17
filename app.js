e401bdab4d59f65b7c413f0373034c8c85e296e1
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-10-18 02:19:49 +0500

    done

---
+++ app.js
@@ -0,0 +1,84 @@
+// PROBLEM 1
+
+// let firstName;
+// let last_name;
+
+// firstName = "Huzaifa"
+// last_name = "Syed"
+// let fullName = firstName + " " + last_name
+
+// console.log("First Name:", firstName)
+// console.log("Last Name:", last_name)
+// console.log("Full Name:", fullName)
+
+// let address = "Main Street 7D2, Karachi"
+// console.log("First Address:", address)
+
+// address = "First Street 7D2, Karachi"
+// console.log("Second Address:", address)
+
+// PROBLEM 2
+
+// let street = "Richards Hill, 11."
+// let country = "United State of America"
+
+// console.log("Street:", street)
+// console.log("Country:", country)
+
+// let fullMailingAddress = street + " " + country 
+// console.log("Full Address by ES 5:", fullMailingAddress)
+
+// let fullAddress = `${street}${country}`
+// console.log("Full Address by ES 6:",fullAddress)
+
+// PROBLEM 3
+
+// let score1 = 90
+// let score2 = 55
+// let score3 = 65
+
+// let totalScore = score1 + score2 + score3
+
+// let average = (score1 + score2 + score3) /3
+
+// console.log("TotalScore:", totalScore)
+// console.log("Average:", average)
+
+// let plates = 20
+// let people = 7
+
+// let remainingPlates = plates - people
+// remainingPlates ++;
+
+// let message = "There are" + " " + remainingPlates + " " + "plates available."
+
+// console.log(message)
+
+// PROBLEM 4
+
+// let fruits = ["apple", "mango" , "orange" , "peach" , "grapes" , "guava" , "kiwi" , 125]
+
+// let firstFruit = fruits[0]
+
+// console.log("Fruits Array before update:", fruits)
+// let last = fruits[fruits.length -1] = "banana"
+
+// console.log("Fruits Array after update:", fruits)
+// console.log("First Fruit:", firstFruit)
+
+// EXTRA PROBLEM 
+
+
+// function twoNumberTheory (a, b) {
+
+// if(a == b){
+//     return console.log("both values are equal")
+// }
+// if(a < b){
+//     console.log(a , " is lesser than" , b)
+// }else {
+//     console.log(b, " is lesser than" , a)
+// }
+// }
+
+// twoNumberTheory(5,17)
